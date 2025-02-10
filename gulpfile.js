e088ed97dd8e24ce43112b7cfae4e50d279e2496
ThebellepoquE <thebellepoque@gmail.com>
2025-02-10 15:12:26 +0100

    Remove tasks.json, add new HTML files, and update gulpfile for improved task handling

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-      directory: true  // Añade esta línea para mostrar directorio
-    },
-    port: 3001,
-    open: true,
-    notify: false,
-    files: ['./**/*.html']
-  });
-
-  gulp.watch(['./**/*.html']).on("change", reload);
+gulp.task('serve', function(done) {
+browserSync.init({
+  server: {
+    baseDir: "./",
+    directory: true  // Añade esta línea para mostrar directorio
+  },
+  port: 3001,
+  open: true,
+  notify: false,
+  files: ['./**/*.html']
 });
+
+gulp.watch(['./**/*.html']).on("change", reload);
+  done();
+});
+
+function defaultTask(done) {
+    done();
+}
+
+exports.default = defaultTask;
